2a062958fea758130e804707d5d9b6b42f99b749
Thisvolwebnik(Nikita) <thisvolwebnik@yandex.ru>
2025-11-03 19:45:24 +0300

    feat: add Questions section with AccordionGroup

---
+++ src/sections/Questions/Questions.jsx
@@ -0,0 +1,33 @@
+import "./Questions.scss";
+import classNames from "classnames";
+import Button from "@/components/Button";
+import Section from "@/layouts/Section";
+import AccordionGroup from "@/components/AccordionGroup";
+
+const Questions = props => {
+  const { className } = props;
+
+  const questionItems = [
+    "What is StreamVibe?",
+    "How much does StreamVibe cost?",
+    "What content is available on StreamVibe?",
+    "How can I watch StreamVibe?",
+    "How do I sign up for StreamVibe?",
+    "What is the StreamVibe free trial?",
+    "How do I contact StreamVibe customer support?",
+    "What are the StreamVibe payment methods?",
+  ];
+  return (
+    <Section
+      className={classNames(className, "question")}
+      title="Frequently Asked Questions"
+      description="Got questions? We've got answers! Check out our FAQ section to find answers to the most common questions about StreamVibe."
+      titleId="questions-title"
+      actions={<Button label="Ask a Question" href="/support" />}
+    >
+      <AccordionGroup columns={2}>{questionItems}</AccordionGroup>
+    </Section>
+  );
+};
+
+export default Questions;
